25261dfe02dc89ad343368b5bcd1b3846e667300
Jake Torres <jakert2244@gmail.com>
2022-05-31 01:47:36 -0400

    Deployment push

---
+++ components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+	Flex,
+	Box,
+	Text,
+	Image,
+	Heading,
+	Button,
+	Spacer
+} from '@chakra-ui/react'
+
+const Contact = () => {
+	return (
+
+	)
+}
+
+export default Contact
--- components/Experience.js
+++ components/Experience.js
@@ -19,7 +19,7 @@ function Feature({title, time, desc, imgUrl, webLink, ...rest}) {
 				<Box p={5}>
 					<Link href={webLink}><Heading fontSize='xl'>{title}</Heading></Link>
 					<Heading size="sm" fontWeight="400" fontStyle="italic">{time}</Heading>
-					<Text mt={4}> {desc} </Text>
+					<Text mt={4} fontSize="14px"> {desc} </Text>
 				</Box>
 			</Flex>
 		</Box>
@@ -29,12 +29,12 @@ function Feature({title, time, desc, imgUrl, webLink, ...rest}) {
 const Experience = () => {
 	return (<section id="experience">
 		<Flex p={5} justifyContent="center" backgroundColor="#ff4066">	
-			<Stack direction="column">
+			<Stack maxWidth="60%" justifyContent="center" direction="column">
 				<Box mb={5}><Heading justifyContent="center" align="center" color="white"> Experience </Heading></Box>
-				<Feature title="Meta University Internship" desc="Work" time="June 2022 - August 2022" imgUrl="/Meta1.png" webLink="https://www.metacareers.com/careerprograms/pathways/metauniversity" />
-				<Feature title="Microsoft TEALs Volunteer" desc="Work 2" time="June 2021 - April 2022" imgUrl="/MS1.png" webLink="https://www.microsoft.com/en-us/teals"/> 
-				<Feature title="Code Ninjas Sensei" desc="Work 3" time="November 2019 - June 2021" imgUrl="/CN2.png" webLink="https://www.codeninjas.com/" />
-				<Feature title="Colorado School of Mines - High School Intern" desc="Work 4" time="June 2020 - August 2020" imgUrl="/CSMines1.jpeg" webLink="https://cs.mines.edu/csminesinterns/"/>  
+				<Feature title="Meta University Internship" desc="I will be working on a team to develop an iOS app using Meta's infastructure and tools." time="June 2022 - August 2022" imgUrl="/Meta1.png" webLink="https://www.metacareers.com/careerprograms/pathways/metauniversity" />
+				<Feature title="Microsoft TEALs Volunteer" desc="Helped high school teachers teach introductory computer science classes. Gave guest speech about my experience." time="June 2021 - April 2022" imgUrl="/MS1.png" webLink="https://www.microsoft.com/en-us/teals"/> 
+				<Feature title="Code Ninjas Sensei" desc="Taught kids 7-15 how to code in JavaScript, Scratch, Python, C#, and Lua by making games. Helped organize and run day to day operations in center. Created curriculum and ran weeklong day camps during the summer and winter breaks. Created and ran a Python class during Covid-19 online learning." time="November 2019 - June 2021" imgUrl="/CN2.png" webLink="https://www.codeninjas.com/" />
+				<Feature title="Colorado School of Mines - High School Intern" desc="Helped run a Python camp for faculty, researchers, professors and teachers around and in Colorado. Wrote curriculum for introductory Python class taken in the folllowing fall semester by undergraduates. Introduced computer science through STEM related fields with an undergraduate partner to K-12 graders. Helped run Coding for Good program where high school students worked on an app to help their community. My team made a carbon footprint calculator website." time="June 2020 - August 2020" imgUrl="/CSMines1.jpeg" webLink="https://cs.mines.edu/csminesinterns/"/>  			
 			</Stack>
 		</Flex>
 	</section>
