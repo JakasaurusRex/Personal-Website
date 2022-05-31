e6085ea5392f21b1f57b30ae80c60896388b7c75
Jake Torres <jakert2244@gmail.com>
2022-05-30 22:29:28 -0400

    Pushing additional items

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 
 import NavBar from '../components/NavBar'
+import About from '../components/About'
+import Experience from '../components/Experience'
 
 export default function Index() {
     return (
 		<>
 			<NavBar> </NavBar>
+			<About> </About>
+			<Experience> </Experience>
 		</>
 	)
 }
